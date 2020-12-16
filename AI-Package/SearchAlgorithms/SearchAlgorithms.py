6d9b773e782345e83200d436c6ec3806497a2676
NayeraMohamed <nayeramoh7@gmail.com>
2020-12-16 16:41:23 +0200

    Update AI-Package/SearchAlgorithms/SearchAlgorithms.py

--- AI-Package/SearchAlgorithms/SearchAlgorithms.py
+++ AI-Package/SearchAlgorithms/SearchAlgorithms.py
@@ -84,7 +84,8 @@ class SearchAlgorithms:
                     self.startNode = maze[i][j]
                 if maze[i][j].value == 'E':
                     self.goalNode = maze[i][j]
-        #clearing path and full path
+        #clearing path and full path from previous calls made
+
         self.path.clear()
         self.fullPath.clear()
 
